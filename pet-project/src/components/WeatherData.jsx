15e0062b11b0f15c3f0ded228b97a80579f99068
Dmitry Kozlov <dima.kozlov.2186@gmail.com>
2022-01-27 18:26:13 +0300

    minor-changes

--- pet-project/src/components/WeatherData.jsx
+++ pet-project/src/components/WeatherData.jsx
@@ -4,19 +4,19 @@ import _ from 'lodash';
 
 
 function WeatherData() {
-    const [data, setData] = React.useState({ hits: [] });
+    const [weatherData, setWeatherData] = React.useState();
 
-    const apiURL = 'http://api.weatherapi.com/v1/current.json?key=3e2b23202a804627a5c115738222701&q=Tula&aqi=no';
+    const apiURL = 'http://api.weatherapi.com/v1/current.json?key=3e2b23202a804627a5c115738222701&q=Moscow&aqi=no';
 
     React.useEffect(() => {
         axios.get(apiURL)
             .then((response) => _.map(response.data, item => _.filter(item,)))
             .then(data => {
                 return (
-                    console.log(data[0][2], data[0][0], data[1][6])
+                    console.log(`Country ` + data[0][2], `City ` + data[0][0], data[1][2], data[1][13])
                 );
             })
-    }, []);
+    }, [setWeatherData]);
     return null;
 };
 
