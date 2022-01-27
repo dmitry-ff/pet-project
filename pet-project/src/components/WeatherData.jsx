4c91cfde1c0eef532893f469f85636dc83364b3e
Dmitry Kozlov <dima.kozlov.2186@gmail.com>
2022-01-27 17:59:05 +0300

    getting weather information

--- pet-project/src/components/WeatherData.jsx
+++ pet-project/src/components/WeatherData.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import axios from 'axios';
+import _ from 'lodash';
 
 
 function WeatherData() {
     const [data, setData] = React.useState({ hits: [] });
-    const apiURL = 'http://api.weatherapi.com/v1/current.json?key=3e2b23202a804627a5c115738222701&q=London&aqi=no';
+
+    const apiURL = 'http://api.weatherapi.com/v1/current.json?key=3e2b23202a804627a5c115738222701&q=Tula&aqi=no';
+
     React.useEffect(() => {
         axios.get(apiURL)
-            .then((response) => console.log(response))
+            .then((response) => _.map(response.data, item => _.filter(item,)))
+            .then(data => {
+                return (
+                    console.log(data[0][2], data[0][0], data[1][6])
+                );
+            })
     }, []);
     return null;
 };
